8fe7a26cf6b925ba197cd9a7cc8e85b1657c1622
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-01-29 06:44:07 +0530

    Update Repo.py

--- MashaRoBot/modules/Repo.py
+++ MashaRoBot/modules/Repo.py
@@ -6,20 +6,20 @@ from MashaRoBot.conf import get_str_key
 from MashaRoBot import pbot
  
  # pls don't delete
-REPO_TEXT = "**Thanimai [BOT](https://telegra.ph/file/526ed899597d7827474a1.jpg) will Make Your Groups Secured And it's have a lot of fun features (:  ! \n\n↼ Owner ⇀ : 『 [Telegram pro](t.me/TheTelegrampro) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THANIMAIBOTS «««"
+REPO_TEXT = "**WolfXRobot [BOT](https://telegra.ph/file/526ed899597d7827474a1.jpg) will Make Your Groups Secured And it's have a lot of fun features (:  ! \n\n↼ Owner ⇀ : 『 [Telegram pro](t.me/HMF_Owner_1) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @WolfXRobot «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("ʀᴇᴘᴏꜱɪᴛᴏʀʏ", url=f"https://github.com/proTamizhan/thanimaibot"),
-        InlineKeyboardButton("Gɪᴛʜᴜʙ", url=f"https://github.com/proTamizhan"),
+        InlineKeyboardButton("ʀᴇᴘᴏꜱɪᴛᴏʀʏ", url=f"https://github.com/Thiruselvan999/God-of-Wolf-X"),
+        InlineKeyboardButton("Gɪᴛʜᴜʙ", url=f"https://github.com/Thiruselvan999"),
       ],[
-        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/TheTelegrampro"),
-        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/thanimaisupport"),
-       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Kittu_the_criminall"),
+        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/HMF_Owner_1"),
+        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/Stuxnet_1_official"),
+       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/stuxnetBotz"),
       ],[
         InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/THANIMAIBOTS"),
-        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/ctzfamily"),
-       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Sarbudeen786"),
+        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/AASFCYBERKING"),
+       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Awesome_Prince"),
       ]]
     )
   
