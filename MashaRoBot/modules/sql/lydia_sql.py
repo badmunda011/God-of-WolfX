84a1909d2a1d61f6ccea5064c71c2984ead968ff
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-23 07:09:14 +0530

    Update lydia_sql.py

--- MashaRoBot/modules/sql/lydia_sql.py
+++ MashaRoBot/modules/sql/lydia_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from MashaRoBot.modules.sql import BASE, SESSION
+from WolfXRobot.modules.sql import BASE, SESSION
 
 
 class ChatbotChats(BASE):
