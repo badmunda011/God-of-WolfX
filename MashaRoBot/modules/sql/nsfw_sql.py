9c0be94e855093fa1d0544451618a2900b1231d1
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-23 07:10:06 +0530

    Update nsfw_sql.py

--- MashaRoBot/modules/sql/nsfw_sql.py
+++ MashaRoBot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from MashaRoBot.modules.sql import BASE, SESSION
+from WolfXRobot.modules.sql import BASE, SESSION
 
 
 class NSFWChats(BASE):
