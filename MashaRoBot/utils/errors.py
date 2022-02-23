2ff769ea51007186e55fa47de141203fcef84b9e
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-23 06:37:05 +0530

    Update errors.py

--- MashaRoBot/utils/errors.py
+++ MashaRoBot/utils/errors.py
@@ -1,10 +1,10 @@
 import sys
 import traceback
 from functools import wraps
-from MashaRoBot import pbot as app
+from WolfXRobot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001162243028)
+LOG_GROUP_ID = int(-1001436797335)
 
 def split_limits(text):
     if len(text) < 2048:
