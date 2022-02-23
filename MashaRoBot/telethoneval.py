4f2863d0afb18afaf77f33f1215332779c0231b4
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-23 06:46:00 +0530

    Update telethoneval.py

--- MashaRoBot/telethoneval.py
+++ MashaRoBot/telethoneval.py
@@ -1,4 +1,4 @@
-from MashaRoBot import telethn as client
+from WolfXRobot import telethn as client
 import os, logging, asyncio, io, sys, traceback
 from datetime import datetime
 from os import environ, execle
