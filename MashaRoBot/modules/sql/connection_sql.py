e8019ddb67d4cd9cabd12f5f91a34bb3c88c4103
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-23 06:57:33 +0530

    Update connection_sql.py

--- MashaRoBot/modules/sql/connection_sql.py
+++ MashaRoBot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from MashaRoBot.modules.sql import SESSION, BASE
+from WolfXRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
