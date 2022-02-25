030b42bd87a1b90dff997aea79b12e565f67312c
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-26 05:28:49 +0530

    Update welcome.py

--- MashaRoBot/modules/welcome.py
+++ MashaRoBot/modules/welcome.py
@@ -5,9 +5,9 @@ import time
 from functools import partial
 from contextlib import suppress
 
-import MashaRoBot.modules.sql.welcome_sql as sql
-import MashaRoBot
-from MashaRoBot import (
+import WolfXRobot.modules.sql.welcome_sql as sql
+import WolfXRobot
+from WolfXRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -19,18 +19,18 @@ from MashaRoBot import (
     dispatcher,
     JOIN_LOGGER
 )
-from MashaRoBot.modules.helper_funcs.chat_status import (
+from WolfXRobot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from MashaRoBot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from MashaRoBot.modules.helper_funcs.msg_types import get_welcome_type
-from MashaRoBot.modules.helper_funcs.string_handling import (
+from WolfXRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from WolfXRobot.modules.helper_funcs.msg_types import get_welcome_type
+from WolfXRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from MashaRoBot.modules.log_channel import loggable
-from MashaRoBot.modules.sql.global_bans_sql import is_user_gbanned
+from WolfXRobot.modules.log_channel import loggable
+from WolfXRobot.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
