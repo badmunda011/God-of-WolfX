2b7e74987f809a414a93a6f14374d360a141ab5d
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-25 22:42:37 +0530

    Update connection.py

--- MashaRoBot/modules/connection.py
+++ MashaRoBot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import MashaRoBot.modules.sql.connection_sql as sql
-from MashaRoBot import dispatcher, DRAGONS, DEV_USERS
-from MashaRoBot.modules.helper_funcs import chat_status
-from MashaRoBot.modules.helper_funcs.alternate import send_message, typing_action
+import WolfXRobot.modules.sql.connection_sql as sql
+from WolfXRobot import dispatcher, DRAGONS, DEV_USERS
+from WolfXRobot.modules.helper_funcs import chat_status
+from WolfXRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
@@ -387,7 +387,7 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "ᴄᴏɴɴᴇᴄᴛ🤞"
+__mod_name__ = "Cᴏɴɴᴇᴄᴛ🤞"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
