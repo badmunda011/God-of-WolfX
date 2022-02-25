31f961ddb0c886b99ceedcdb18fecad7b46924d4
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-26 04:40:53 +0530

    Update whatanime.py

--- MashaRoBot/modules/whatanime.py
+++ MashaRoBot/modules/whatanime.py
@@ -16,7 +16,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import is_image, is_video
 
-from MashaRoBot.events import register as tomori
+from WolfXRobot.events import register as tomori
 
 
 
