186d887ea775e12a5758195ab99003341a414064
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-26 07:47:44 +0530

    Update users.py

--- MashaRoBot/modules/users.py
+++ MashaRoBot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import MashaRoBot.modules.sql.users_sql as sql
-from MashaRoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from MashaRoBot.modules.sql.users_sql import get_all_users
+import WolfXRobot.modules.sql.users_sql as sql
+from WolfXRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from WolfXRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from WolfXRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
