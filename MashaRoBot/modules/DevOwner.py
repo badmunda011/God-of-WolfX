6d34de021e6df46ad076cf2eab0fd2b7f5037513
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-26 17:24:52 +0530

    Delete MashaRoBot directory

--- MashaRoBot/modules/DevOwner.py
+++
@@ -1,8 +0,0 @@
-__help__= f"""
-**ᴀʟʟ  ᴄʀᴇᴀᴅɪᴛs  ᴛᴏ 
-       👑 [✰ ꫝꪖᥴ𝕜ꫀ𝕣 ✰](https://t.me/HMF_OWNER_1) ❤️
-       👑 [『 𝙆𝙄𝙎𝙃𝙊𝙍𝙀™』](https://t.me/AASFCYBERKING) ❤️
-       👑 [「 𝐋𝐨𝐯𝐞𝐥𝐲 𝐏𝐫𝐢𝐧𝐜𝐞™ 」](https://t.me/Awesome_Prince) ❤️**
-"""
-
-__mod_name__ = "Cʀᴇᴀᴅɪᴛs🥵"
--- MashaRoBot/modules/chatbots.py
+++
@@ -1,7 +0,0 @@
-__help__= f"""
-*Thanimai AI * 
-/addchat : Enables and Disables kazuko AI Chat mode (EXCLUSIVE)
-*Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
-"""
-
-__mod_name__ = "Cʜᴀᴛʙᴏᴛ🤖"
