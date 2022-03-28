69e5e05767978b83c0a69ce32605f48bf2a84f09
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-03-28 17:36:38 +0530

    Update obama.py

--- WolfXRobot/modules/obama.py
+++ WolfXRobot/modules/obama.py
@@ -1,13 +1,16 @@
+from typing import List
 from telethon import custom, events
 import os,re
 import asyncio
+from telegram import Update
+from telegram.ext import run_async,CallbackContext
 import time
-from pyrogram import filters
-from pyrogram.types import Message
-from WolfXRobot import telethn as bot
-from WolfXRobot import telethn as tgbot
-from WolfXRobot import telethn as aasf
 from WolfXRobot.events import register
+WolfXRobot import telethn as tgbot
+from pyrogram import filters
+from WolfXRobot import dispatcher
+from WolfXRobot import telethn as bot
+from WolfXRobot.modules.disable import DisableAbleCommandHandler
 
 @register(pattern="/obama")
 async def status(bot, m: Message):
