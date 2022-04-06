353ab6c9cd8975201cbb72f647a511374529f66c
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-07 05:29:28 +0530

    Update __main__.py

--- WolfXRobot/__main__.py
+++ WolfXRobot/__main__.py
@@ -303,7 +303,7 @@ def start(update: Update, context: CallbackContext):
              first_name = update.effective_user.first_name
              update.effective_message.reply_photo(
              random.choice(AASF),
-             caption=(PM_START_TEXT).format(
+             (PM_START_TEXT).format(
                     escape_markdown(first_name),
                     escape_markdown(uptime),
                     sql.num_users(),
