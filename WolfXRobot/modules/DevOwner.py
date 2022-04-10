29bdb289c6857e68c2719943d02f5c869074e90b
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-04-10 08:27:01 +0530

    Update DevOwner.py

--- WolfXRobot/modules/DevOwner.py
+++ WolfXRobot/modules/DevOwner.py
@@ -1,8 +1,8 @@
 __help__= f"""
 **ᴀʟʟ  ᴄʀᴇᴀᴅɪᴛs  ᴛᴏ 
-       👑 [✰ ꫝꪖᥴ𝕜ꫀ𝕣 ✰](https://t.me/HMF_OWNER_1) ❤️
-       👑 [『 𝙆𝙄𝙎𝙃𝙊𝙍𝙀™』](https://t.me/AASFCYBERKING) ❤️
-       👑 [「 𝐋𝐨𝐯𝐞𝐥𝐲 𝐏𝐫𝐢𝐧𝐜𝐞™ 」](https://t.me/Awesome_Prince) ❤️**
+       🎉 [✰ ꫝꪖᥴ𝕜ꫀ𝕣 ✰](https://t.me/HMF_OWNER_1) ❤️
+       🎉 [『 𝙆𝙄𝙎𝙃𝙊𝙍𝙀™』](https://t.me/AASF_CYBERKING) 🖤
+       🎉 [「 𝐋𝐨𝐯𝐞𝐥𝐲 𝐏𝐫𝐢𝐧𝐜𝐞™ 」](https://t.me/DarlingPrince) 💜**
 """
 
 __mod_name__ = "Cʀᴇᴀᴅɪᴛs🥵"
