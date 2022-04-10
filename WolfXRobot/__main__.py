0237f2af11fdc093810e0c2463e4e4430521131f
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-10 18:35:22 +0530

    Update __main__.py

--- WolfXRobot/__main__.py
+++ WolfXRobot/__main__.py
@@ -341,10 +341,8 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
-            ] 
-                ]
-            ),
-        )
+              )
+
 
 def error_handler(update, context):
     """Log the error and send a telegram message to notify the developer."""
