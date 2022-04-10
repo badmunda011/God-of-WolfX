65d01a5c0e4dbc8e0425d74df744c95b43ab03e2
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-10 05:44:11 +0530

    Update __main__.py

--- WolfXRobot/__main__.py
+++ WolfXRobot/__main__.py
@@ -408,8 +408,8 @@ def help_button(update, context):
                 )
                 + HELPABLE[module].__help__
             )
-            query.message.edit_text(
-                text=text,
+            query.message.edit_caption(
+                caption=text,
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
