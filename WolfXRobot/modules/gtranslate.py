6b1d67261feedf59543662b5e0637e1757f49da8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 07:29:37 +0530

    Create gtranslate.py

---
+++ WolfXRobot/modules/gtranslate.py
@@ -0,0 +1,52 @@
+from wolfXRobot import pbot as bot
+from pyrogram import filters
+from gpytranslate import Translator
+
+@bot.on_message(filters.command(["lang", "langs"]))
+def language(_, m):
+       #langs codes
+        m.reply_text("Click on the button below to see the list of supported language codes.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Language codes",
+                        url="https://telegra.ph/Lang-Codes-03-19-3",
+                    ),
+                ],
+            ],
+        ),
+    )
+
+
+trans = Translator()
+
+
+@bot.on_message(filters.command(["tl", "tr"]))
+async def translate(_, message: Message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("Reply to a message to translate it!\n Use: /langs for translation codes")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"**Translated from {source} to {dest}**:\n"
+        f"`{translation.text}`"
+    )
+
+    await message.reply_text(reply)
