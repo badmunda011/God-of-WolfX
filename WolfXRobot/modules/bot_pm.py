f31268109f98e0e83c3617ea5dfe179f7195a0d3
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-11-20 16:30:44 +0530

    Update bot_pm.py

--- WolfXRobot/modules/bot_pm.py
+++ WolfXRobot/modules/bot_pm.py
@@ -13,7 +13,7 @@ from WolfXRobot import OWNER_ID
     & filters.incoming
  )
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 1989750989:
+    if not message.from_user.id == 960432019:
         fwded_mesg = await message.forward(
             chat_id=OWNER_ID,
             disable_notification=True
